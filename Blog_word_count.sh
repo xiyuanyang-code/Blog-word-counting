dbd882cdc5386b845747a5120ad758ab2bbaa016
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-05-13 14:07:50 +0800

    refactor!: Change file structures

--- Blog_word_count.sh
+++
@@ -1,8 +0,0 @@
-BLOG_WORD(){
-    # run the python file
-    cd ~/Hodgepodge/Blog-word-counting
-    /home/xiyuanyang/anaconda3/bin/python ~/Hodgepodge/Blog-word-counting/main.py;
-    cat ~/Hodgepodge/Blog-word-counting/total.json | tail -5 | head -3
-    echo "\nDone!"
-    cd -
-}
