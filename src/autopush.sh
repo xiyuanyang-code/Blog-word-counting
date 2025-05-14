f5421983f00918796d4878e5aabc7522cb9dcd0e
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-05-14 23:44:03 +0800

    Auto-commit-update: 2025-05-14 23:44:03

--- src/autopush.sh
+++ src/autopush.sh
@@ -6,14 +6,14 @@ MARKER_FILE=~/.last_push_date
 if [[ ! -f "$MARKER_FILE" || $(date +%Y-%m-%d) != $(cat "$MARKER_FILE") ]]; then
     cd "$REPO_DIR" || exit 1
     # run the bash scripts, but not outputting anything
-    bash src/run.sh >/dev/null 2>&1
+    bash src/run.sh 2> /dev/null
 
     if git status --porcelain | grep -q '^ M\|^M \|^A \|^D \|^\?\?'; then
         git add .
         git commit -m "Auto-commit-update: $(date +'%Y-%m-%d %H:%M:%S')"
     fi
 
-    if git push >/dev/null 2>&1; then
+    if git push 2> /dev/null; then
         date +%Y-%m-%d >"$MARKER_FILE"
     else
         echo "[Auto-Push] Failed to push. Will retry later."
