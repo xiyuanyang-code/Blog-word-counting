dd4cadfcc3e5fd06e684e545b1ee59e664af4b56
xiyuanyang-code <xiyuan__yang@outlook.com>
2025-08-03 18:34:54 +0800

    Upd: 2025-08-03 18:34:54

--- src/run.sh
+++ src/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ../Blog-word-counting
+cd ../Blog-word-counting || exit
 python ./src/main.py
 cat ./total.json | tail -5 | head -3
 echo "Done!"
@@ -9,4 +9,4 @@ git add .
 git commit -m "Upd: $(date '+%Y-%m-%d %H:%M:%S')"
 git push
 
-cd -
+cd - || exit
